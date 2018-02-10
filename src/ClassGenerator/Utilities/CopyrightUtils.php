0bfae8a251aa53c26a2e5f9e710856bc5536c6f7
Daniel Paul Carbone <dcarbone@ena.com>
2018-02-10 17:01:26 -0600

    updating copyright date

--- src/ClassGenerator/Utilities/CopyrightUtils.php
+++ src/ClassGenerator/Utilities/CopyrightUtils.php
@@ -55,7 +55,7 @@ abstract class CopyrightUtils
             '',
             'PHPFHIR Copyright:',
             '',
-            'Copyright 2016-2017 Daniel Carbone (daniel.p.carbone@gmail.com)',
+            'Copyright 2016-2018 Daniel Carbone (daniel.p.carbone@gmail.com)',
             '',
             'Licensed under the Apache License, Version 2.0 (the "License");',
             'you may not use this file except in compliance with the License.',
