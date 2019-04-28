f6d448e5a7fe3a9025adaaa1a26cbb834599451d
Daniel Paul Carbone <dcarbone@ena.com>
2019-04-28 11:43:58 -0500

    some cleanup around namespace setting in config and beginning work on helpers class

--- template/utilities/helper_class.php
+++ template/utilities/helper_class.php
@@ -16,11 +16,46 @@
  * limitations under the License.
  */
 
+use DCarbone\PHPFHIR\Enum\TypeKindEnum;
 use DCarbone\PHPFHIR\Utilities\CopyrightUtils;
 
 /** @var \DCarbone\PHPFHIR\Config\VersionConfig $config */
+/** @var \DCarbone\PHPFHIR\Definition\Types $types */
+
+// find either Resource Container or Inline.Resource
+
+$containerType = null;
+foreach ($types->getIterator() as $type) {
+    $kind = $type->getKind();
+    if ($kind->isResourceContainer() || $kind->isInlineResource()) {
+        $containerType = $type;
+        break;
+    }
+}
+
+if (null === $containerType) {
+    throw new \RuntimeException(sprintf(
+            'Unable to locate either "%s" or "%s" type',
+        TypeKindEnum::RESOURCE_CONTAINER,
+        TypeKindEnum::RESOURCE_INLINE
+    ));
+}
 
 $namespace = $config->getNamespace(false);
+
+$imports = [];
+foreach ($containerType->getProperties()->getIterator() as $property) {
+    $ptype = $property->getValueFHIRType();
+    if (null === $ptype) {
+        continue;
+    }
+    if ($ptype->getFullyQualifiedNamespace(false) !== $namespace) {
+        $imports[] = $ptype->getFullyQualifiedClassName(false);
+    }
+}
+
+$imports = array_unique($imports);
+natcasesort($imports);
 
 ob_start();
 
@@ -34,15 +69,19 @@ echo CopyrightUtils::getFullPHPFHIRCopyrightComment();
 
 echo "\n\n";
 
+foreach ($imports as $import) :
+    echo "use {$import};\n";
+endforeach;
 
+echo "\n";
 
 ?>
 /**
  * Class PHPFHIRHelpers<?php if ('' !== $namespace) : ?>
 
-
- * @package \\<?php echo $namespace; ?>
+ * @package \<?php echo $namespace; ?>
 <?php endif;?>
+
  */
 abstract class PHPFHIRHelpers
 {
